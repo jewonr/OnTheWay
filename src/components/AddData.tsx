5e1673fcc9987ee679adfc164d8431ec37a1ad68
Na jewon <80616120+jewonr@users.noreply.github.com>
2022-09-18 20:38:28 +0900

    update view height

--- src/components/AddData.tsx
+++ src/components/AddData.tsx
@@ -15,6 +15,7 @@ type AddCategoryType = {
 const Container = styled.div`
   border-top: 1px solid #E1E1E1;
   width: 100%;
+  height: 70vh;
   padding: 20px;
   transition: .5s;
   background: #F9F9F9;
